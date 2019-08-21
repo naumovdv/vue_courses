f186ed6b36fd6bd5a7cb1ccfb15fc1c13ec7a6f2
Dmitriy Naumov <naumov@uchi.ru>
2019-08-21 22:22:23 +0400

    Axios, user fields, bootstrap styles

--- userList/scripts/app.js
+++ userList/scripts/app.js
@@ -6,7 +6,8 @@
         title: 'Пользователи',
         defaultPhoto: 'https://freelance.ru/img/avatars/1960013.png?030000',
         toggleButtonTooltip: 'Показать/скрыть список',
-        isVisibleUserList: true
+        isVisibleUserList: true,
+        countFieldsOfUserVisible: 9
       }
     },
     props: {
@@ -18,17 +19,42 @@
     methods: {
       getUserFullName: function (id) {
         const user = this.users.find(user => user.id === id);
-        return user ? `${user.surname} ${user.name} ${user.patronymic}` : '';
+        return user ? `${user.firstName} ${user.lastName}` : '';
       },
       getUserPhotoURL: function (id) {
         const user = this.users.find(user => user.id === id);
-        return user && user.photo ? user.photo : this.defaultPhoto;
+        return user && user.picture ? user.picture : this.defaultPhoto;
       },
       toggleUserList: function () {
         this.isVisibleUserList = !this.isVisibleUserList;
+      },
+      isUserFieldByIndex: function (index) {
+        return index <= this.userFields.length;
+      },
+      getContentByFieldIndexAndUser: function(index, user) {
+        const contentType = this.userFields[index];
+        const contentByType = {
+          picture: () => `<img src="${this.getUserPhotoURL(user.id)}" alt="${this.getUserFullName(user.id)}" title="${this.getUserFullName(user.id)}">`
+        };
+
+        return contentByType.hasOwnProperty(contentType)
+          ? contentByType[contentType]()
+          : user[this.userFields[index]];
       }
     },
     computed: {
+      userFields: function() {
+        const fields = [];
+        if (this.users.length) {
+          const user = this.users[0];
+          for (const key in user) {
+            if (user.hasOwnProperty(key)) {
+              fields.push(key);
+            }
+          }
+        }
+        return fields;
+      },
       countUsers: function () {
         return this.users.length;
       },
@@ -58,43 +84,15 @@
       }
     },
     mounted: function() {
-      this.users = [
-        {
-          id: 1,
-          name: 'Иван',
-          surname: 'Иванов',
-          patronymic: 'Иванович',
-          photo: 'https://teinon.net/ficbook/uploads/avatars/avatar_-N.e.g.aTiV-_1534262251.png'
-        },
-        {
-          id: 2,
-          name: 'Петр',
-          surname: 'Петров',
-          patronymic: 'Петрович',
-          photo: 'https://teinon.net/ficbook/uploads/avatars/avatar_nailika_1553198948.png'
-        },
-        {
-          id: 3,
-          name: 'Александр',
-          surname: 'Александров',
-          patronymic: 'Александрович',
-          photo: 'https://teinon.net/ficbook/uploads/avatars/avatar_Pa7if_1531912455.png'
-        },
-        {
-          id: 4,
-          name: 'Павел',
-          surname: 'Павлов',
-          patronymic: 'Павлович',
-          photo: 'https://id-static.z-dn.net/files/d2f/dc625b5a5d5edf12df7cac76c2b98b1f.png'
-        },
-        {
-          id: 5,
-          name: 'Андрей',
-          surname: 'Андреев',
-          patronymic: 'Андреевич'
-        }
-      ];
-      console.warn('Users is loaded'); 
+      this.users = [];
+      axios
+        .get('http://localhost:3000/users')
+        .then(response => {
+          this.users = response.data;
+          console.warn('Users is loading');
+        })
+        .catch(error => console.error(error))
+        .finally(() => console.warn('Loading Done'));
     }
   });
 }());
